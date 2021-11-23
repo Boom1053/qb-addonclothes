eacae760773dc3fab1aa8d4d40d8629a78e4fbab
Boom <42920111+Boom1053@users.noreply.github.com>
2021-11-22 17:10:01 -0800

    Thank Mazkee

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Big Dick Mazkee'
+discord 'Mazkee#7791'
+
+files {
+  "mp_f_freemode_01_qb_shop.meta",
+  "mp_m_freemode_01_qb_shop.meta"
+}
+
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_f_freemode_01_qb_shop.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_m_freemode_01_qb_shop.meta"
